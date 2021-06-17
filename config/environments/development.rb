b3632c91060e759550b3afc117b7858f929f34d5
damari <motokin128@gmail.com>
2021-06-17 12:48:15 +0900

    factory_bot_rails を設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # 追加  コンソールから factory_bot_rails を使用するため
+  config.factory_bot.definition_file_paths = ["spec/factories"] 
 end
