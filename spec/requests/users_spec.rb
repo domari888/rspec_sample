18336f0e2fe7c42aa2ddd3953db9ad16e66ac9e4
damari <motokin128@gmail.com>
2021-06-18 21:26:02 +0900

    show のリクエストスペックを実装

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,4 +16,40 @@ RSpec.describe "Users", type: :request do
       end
     end
   end
+
+  describe "GET #show" do
+    subject { get(user_path(user_id)) }
+    context "ユーザーが存在するとき" do
+      let(:user) { create(:user) }
+      let(:user_id) { user.id }
+
+      it "リクエストが存在する" do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "name が表示される" do
+        subject
+        expect(response.body).to include user.name
+      end
+
+      it "age が表示される" do
+        subject
+        expect(response.body).to include user.age.to_s
+      end
+
+      it "email が表示される" do
+        subject
+        expect(response.body).to include user.email
+      end
+
+    end
+
+    context ":id に対するユーザーが存在しないとき" do
+      let(:user_id) { 1 }
+      it "エラーが発生する" do
+        expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
 end
